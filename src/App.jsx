a84d7a7a7dbc22b07b6277dc62f83ac3d4c6192c
ikana82 <ikanurasih82@gmail.com>
2025-12-28 21:15:06 +0700

    add description of The Lion

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ const sliderData = [
       "https://images.unsplash.com/photo-1703248187251-c897f32fe4ec?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "The Giant Panda",
     description:
-      "The giant panda is a striking bear species native to the high-altitude bamboo forests of central China, instantly recognizable by its bold black-and-white coat and distinctive dark eye patches. Although they belong to the order Carnivora, these gentle giants are almost exclusively herbivorous, spending up to fourteen hours a day consuming vast amounts of bamboo to maintain their energy. Beyond their famous appearance, pandas possess unique biological adaptations, such as an elongated wrist bone that functions like a thumb for gripping stalks. Known for their solitary and peaceful temperament, they have become a global icon for wildlife conservation, representing both the beauty of nature and the ongoing efforts to protect endangered habitats.",
+      "The giant panda is a striking bear species native to the high altitude bamboo forests of central China, instantly recognizable by its bold black and white coat and distinctive dark eye patches. Although they belong to the order Carnivora, these gentle giants are almost exclusively herbivorous, spending up to fourteen hours a day consuming vast amounts of bamboo to maintain their energy. Beyond their famous appearance, pandas possess unique biological adaptations, such as an elongated wrist bone that functions like a thumb for gripping stalks. Known for their solitary and peaceful temperament, they have become a global icon for wildlife conservation, representing both the beauty of nature and the ongoing efforts to protect endangered habitats.",
   },
   {
     image:
       "https://images.unsplash.com/photo-1546182990-dffeafbe841d?q=80&w=2059",
     title: "The Majestic Lion",
-    description: "",
+    description:
+      "The majestic lion (Panthera leo), often hailed as the 'King of Beasts,' is a powerful symbol of strength and courage that reigns over the savannas and grasslands of sub-Saharan Africa. Distinguished by the male’s thick, regal mane which darkens with age and serves as a badge of dominance these apex predators are unique among big cats for their complex social structure called a pride. While the lionesses are the primary hunters, utilizing strategic teamwork to take down large prey like zebras and wildebeests, the males are responsible for protecting the pride's territory with their thunderous roars that can be heard up to five miles away. Despite their fierce reputation, lions are remarkably leisurely animals, spending up to twenty hours a day resting under the golden sun, embodying a blend of raw power and quiet dignity.",
   },
   {
     image: "",
