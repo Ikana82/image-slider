fe26854a42061f1414968322c9aa17a427839064
ikana82 <ikanurasih82@gmail.com>
2025-12-28 21:16:15 +0700

    add image elephant

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,9 @@ const sliderData = [
       "The majestic lion (Panthera leo), often hailed as the 'King of Beasts,' is a powerful symbol of strength and courage that reigns over the savannas and grasslands of sub-Saharan Africa. Distinguished by the male’s thick, regal mane which darkens with age and serves as a badge of dominance these apex predators are unique among big cats for their complex social structure called a pride. While the lionesses are the primary hunters, utilizing strategic teamwork to take down large prey like zebras and wildebeests, the males are responsible for protecting the pride's territory with their thunderous roars that can be heard up to five miles away. Despite their fierce reputation, lions are remarkably leisurely animals, spending up to twenty hours a day resting under the golden sun, embodying a blend of raw power and quiet dignity.",
   },
   {
-    image: "",
-    title: "",
+    image:
+      "https://images.unsplash.com/photo-1575187105891-be9b5d30cecd?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "The Wild Elephant",
     description: "",
   },
 ];
